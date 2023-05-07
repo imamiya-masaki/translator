2d44758b114ba5489e559491c362187cebbb7893
imamiya-masaki <imamiyagit@gmail.com>
2023-05-07 12:48:33 +0900

    wip

---
+++ apps/client/src/app/auth/layout.tsx
@@ -0,0 +1,16 @@
+import './auth.css'
+
+export default function AuthLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <div>
+        <div>
+
+        </div>
+      </div>
+    )
+  }
+  
